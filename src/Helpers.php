ef3f3dbdfe302a53b5ea161da64c67467ab9f80a
Dusan Malusev <dusan@dusanmalusev.dev>
2024-04-04 11:50:46 +0200

    feat: setup pest as testing tool
    Signed-off-by: Dusan Malusev <dusan@dusanmalusev.dev>

--- src/Helpers.php
+++ src/Helpers.php
@@ -6,21 +6,26 @@ namespace Codelieutenant\LaravelPgenum;
 
 use Codelieutenant\LaravelPgenum\Exceptions\InvalidEnumType;
 use Illuminate\Database\Connection;
+use Illuminate\Database\Schema\Grammars\Grammar;
 use Illuminate\Support\Str;
 use ReflectionEnum;
+use ReflectionException;
 use StringBackedEnum;
 use UnitEnum;
 
 final class Helpers
 {
-    public static function formatNameForDatabase(Connection $conn, string $name): string
+    /**
+     * @throws ReflectionException
+     */
+    public static function formatNameForDatabase(Grammar $grammar, string $name): string
     {
         if (class_exists($name)) {
             $reflection = new ReflectionEnum($name);
             $name = Str::snake($reflection->getName());
         }
 
-        return $conn->getSchemaGrammar()->wrap($name);
+        return $grammar->wrap($name);
     }
 
     public static function formatValuesForDatabase(Connection $conn, array $values): string
